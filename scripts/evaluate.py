c9304673cb08b67e29ff1440725179fef966ba12
刘沛霖 <136075808+JLU-Liupeilin@users.noreply.github.com>
2026-09-10 16:37:02 +0800

    Delete scripts directory

--- scripts/evaluate.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from narracrime_evar.cli import evaluate_main
-
-if __name__ == "__main__":
-    evaluate_main()
--- scripts/run_evar.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from narracrime_evar.cli import run_main
-
-if __name__ == "__main__":
-    run_main()
